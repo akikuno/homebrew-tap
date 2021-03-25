ded16a553b78b670e85d6f67fa794b27c7bf2b8c
akikuno <akikuno@users.noreply.github.com>
2021-03-25 16:23:20 +0900

    cretate Formula

---
+++ Formula/test_echo.rb
@@ -0,0 +1,35 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class TestEcho < Formula
+  desc ""
+  homepage ""
+  url "https://github.com/akikuno/test_brew/archive/refs/tags/0.0.1.tar.gz"
+  sha256 "c1de6de8583ba5469aa894f4177fec35c76de08b0a6f55239b5a164049feb27d"
+  license "MIT"
+
+  # depends_on "cmake" => :build
+
+  def install
+    # ENV.deparallelize  # if your formula fails when building in parallel
+    # Remove unrecognized options if warned by configure
+    system "./configure", "--disable-debug",
+                          "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--prefix=#{prefix}"
+    # system "cmake", ".", *std_cmake_args
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test test_echo`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # homebrew-tap
- 
+
+>Homebrewでformulaの作成
+>https://blog.ottijp.com/2020/05/23/homebrew-make-formula/
+
+brew tap-new akikuno/tap
+
+brew create --tap akikuno/tap \
+--set-name test_echo "https://github.com/akikuno/test_brew/archive/refs/tags/0.0.1.tar.gz"
+
+export HOMEBREW_EDITOR="code"
+brew edit akikuno/tap/test_echo
